f5b3b5442b975fcfdb0feabb2ddbf915c4a31e49
jeffersoncsw <je_wiltenburg@hotmail.com>
2023-05-08 10:44:16 -0300

    Ajustando importações

--- src/test/java/com/atendimento/api/service/AtendimentoServiceTest.java
+++ src/test/java/com/atendimento/api/service/AtendimentoServiceTest.java
@@ -16,12 +16,10 @@ import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.OffsetDateTime;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
